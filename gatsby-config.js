8c795fb61b394f236c5b9aac58ee10f886c3d82c
Anand Poolla <anandpoolla@gmail.com>
2020-07-10 16:13:50 -0400

    Add gatsby remark transformer plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,8 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/hot-pepper.png` // This path is relative to the root of the site.
       }
-    }
+    },
+    `gatsby-transformer-remark`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
