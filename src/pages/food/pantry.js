4f9c38a37e1ee04783d12a1e538939d066315c33
Anand Poolla <anandpoolla@gmail.com>
2020-07-18 20:31:36 -0400

    Add starter pantry items and more details on make-this-site blog

--- src/pages/food/pantry.js
+++ src/pages/food/pantry.js
@@ -11,7 +11,35 @@ export default function Pantry() {
       ]}
     >
       <h1>Pantry</h1>
-      These are the basics you will need
+      <div>
+        You need to stock these items in your pantry for the recipes I'm going to post. I highly recommend searching
+        for the nearest Indian store to get all the items in one trip.
+
+        <h2>Tempering spices</h2>
+        <ul>
+          <li>Turmeric</li>
+          <li>Mustard seeds</li>
+          <li>Cumin seeds</li>
+          <li>Dried red chilies</li>
+          <li>Asafoetida</li>
+        </ul>
+
+        <h2>Lentils</h2>
+        <ul>
+          <li>Split pigeon pea / Kandhi pappu (Telugu) / Toor dal (Hindi)</li>
+          <li>Split black gram / Minapa pappu (Telugu) / Urad dal (Hindi)</li>
+          <li>Split Bengal gram / Senaga pappu (Telugu) / Channa dal (Hindi)</li>
+        </ul>
+
+        <h2>Masala spices</h2>
+        <ul>
+          <li>Red chili powder</li>
+          <li>Cloves</li>
+          <li>Cardamom</li>
+          <li>Cinnamon bark</li>
+          <li>Star anise</li>
+        </ul>
+      </div>
     </Layout>
   );
 }
