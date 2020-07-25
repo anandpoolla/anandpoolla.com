e32b243d867f2a16a734d77634dfe607e908a929
Anand Poolla <anandpoolla@gmail.com>
2020-07-25 18:47:47 -0400

    Add prism js for markdown code themes

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,13 +43,44 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          `gatsby-remark-autolink-headers`,
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+              languageExtensions: [
+                {
+                  language: "superscript",
+                  extend: "javascript",
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+              prompt: {
+                user: "root",
+                host: "localhost",
+                global: false
+              },
+              escapeEntities: {},
+            }
+          },
           {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1024
             }
           },
-          `gatsby-remark-autolink-headers`
+
         ]
       }
     },
