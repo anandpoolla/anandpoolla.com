10a4a966bc91dd590b25384c3184081378f0fb5b
Anand Poolla <anandpoolla@gmail.com>
2022-07-23 14:43:31 -0400

    Remove prefetch Google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,7 +107,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
           {
